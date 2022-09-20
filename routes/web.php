3be3a997e488f11aca8c6e4908815c524fbf5313
haruk0403 <haruka_rocky322@yahoo.co.jp>
2022-09-20 22:02:23 +0900

    2022-9-20

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::prefix('items')->group(function () {
     Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
     Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
 });
+Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('user');
+Route::get('/user/edit/{id}', [App\Http\Controllers\UserController::class, 'edit'])->name('user.edit');
+Route::post('/user/edit/{id}', [App\Http\Controllers\UserController::class,'postEdit'])->name('users.postEdit');
+Route::post('/user/postEdit',[\App\Http\Controllers\UserController::class,'postEdit'])->name('postEdit');
